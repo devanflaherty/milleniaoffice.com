3fa9a8791eb3bb9e20eff277e6cae89037ef3af5
Devan Flaherty <devanflaherty@me.com>
2018-01-12 13:02:04 -0700

    run build

--- static/sw.js
+++ static/sw.js
@@ -8,40 +8,40 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.e2c2b302524f0e228290.js",
-    "revision": "a1ea327147083b099896264531ce17ab"
+    "url": "/_nuxt/app.d5aba8d151988d5eae6d.js",
+    "revision": "dfaf5436c404296b430ea5d5b232af8c"
   },
   {
     "url": "/_nuxt/layouts/default.3996530668a342c33540.js",
     "revision": "e4a92c4c8e984b2cfbdee29b01eceedb"
   },
   {
-    "url": "/_nuxt/manifest.e165ed7e3d1add66ef1e.js",
-    "revision": "04cced52c4bdccd084c07f4e2518099d"
+    "url": "/_nuxt/manifest.0bbb710ffcf6de20448b.js",
+    "revision": "809bbf2554089a3ff21ffaaca1e1e748"
   },
   {
-    "url": "/_nuxt/pages/_page/_child.548957fe7034542bd197.js",
-    "revision": "7e8b21b009c83d375723017939bfb208"
+    "url": "/_nuxt/pages/_page/_child.4c110053563c7cc0e382.js",
+    "revision": "cc9eb06c84d9a5faea1ab5a18831d0aa"
   },
   {
-    "url": "/_nuxt/pages/_page/index.161b23392cf963484d64.js",
-    "revision": "c9d70606d9164d37809660796d7588f9"
+    "url": "/_nuxt/pages/_page/index.9928f55aaecffd3d1097.js",
+    "revision": "13e59553f798b14b76975c7a29338122"
   },
   {
-    "url": "/_nuxt/pages/contact.6f620c20b658da92f957.js",
-    "revision": "baa14d6c150f865b002513e4b3b91846"
+    "url": "/_nuxt/pages/contact.734b0a239a6afcbcf046.js",
+    "revision": "35d5e9aba1be41c9b065dd3145345fc6"
   },
   {
-    "url": "/_nuxt/pages/index.d51855de307fc731ad94.js",
-    "revision": "c2c2af838f685dd041ecd7db2bce90e8"
+    "url": "/_nuxt/pages/index.490ab63b92c01adfe366.js",
+    "revision": "ab93b97b06aca9bde547a3c21cf66d06"
   },
   {
     "url": "/_nuxt/pages/preview.b123f3d34b16c1ed80c2.js",
     "revision": "75c9b648e3c1b3aae08a2865a7b7efcc"
   },
   {
-    "url": "/_nuxt/vendor.646a65afd8d126559d0e.js",
-    "revision": "09266681315d9f6d12a1c3f62696fc38"
+    "url": "/_nuxt/vendor.922f9915334ea0799bad.js",
+    "revision": "be3748836241f0998fd071b37c29e4b4"
   }
 ])
 
